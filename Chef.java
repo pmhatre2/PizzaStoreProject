1323b8b37f1fe8f0ff76966b1ccd0dbd2dbb7276
pmhatre2 <117546590+pmhatre2@users.noreply.github.com>
2022-11-07 20:01:01 -0700

    Add files via upload

---
+++ Chef.java
@@ -0,0 +1,21 @@
+package projectII;
+
+public class Chef {
+	private int ID;
+
+	public Chef(int id) {
+		this.ID = id;
+	}
+	
+	public void setID(int id) {
+		this.ID = id;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public void setOrderStatus(String status) {
+		
+	}
+}
