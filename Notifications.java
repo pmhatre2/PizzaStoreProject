47ec4c3ef60ba35006082d277d408e6f44226203
Ryan El-Aawar <111483752+RyanLOR@users.noreply.github.com>
2022-11-13 10:26:38 -0700

    Add files via upload

--- Notifications.java
+++ Notifications.java
@@ -1,12 +1,129 @@
-package projectII;
+package Project;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class Notifications {
 	
-	public boolean checkValidID(int id) {
-	return true;
+	public static boolean checkValidIDStudent(String id) {
+		try {
+			FileReader fr = new FileReader("StudentIDs.txt");
+	        BufferedReader br = new BufferedReader(fr);
+	        String line;
+	        
+	        while ((line = br.readLine()) != null) {
+	            if (line.contains(id)) {
+	            	br.close();
+	            	return true;
+	            }
+	        }
+	        br.close();
+	        return false;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
-	public void sendEmail(Student student) {
-		
+	public static boolean checkValidIDChef(String id) {
+		try {
+			FileReader fr = new FileReader("ChefIDs.txt");
+	        BufferedReader br = new BufferedReader(fr);
+	        String line;
+	        
+	        while ((line = br.readLine()) != null) {
+	            if (line.contains(id)) {
+	            	br.close();
+	            	return true;
+	            }
+	        }
+	        br.close();
+	        return false;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static boolean checkValidIDProcessingAgent(String id) {
+		try {
+			FileReader fr = new FileReader("ProcessingAgentIDs.txt");
+	        BufferedReader br = new BufferedReader(fr);
+	        String line;
+	        
+	        while ((line = br.readLine()) != null) {
+	            if (line.contains(id)) {
+	            	br.close();
+	            	return true;
+	            }
+	        }
+	        br.close();
+	        return false;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static void writeOrder(Order order) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("OrderList.txt"));
+	        oos.writeObject(order);
+	        //oos.flush();
+	        oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		/*String input = order.toString();
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("acceptedOrders.txt"));
+		    out.write(input); 
+		    out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} */
+	}
+	
+	public ArrayList<Order> readFile(String file) {
+		ArrayList<Order> ret = new ArrayList<Order>();
+		boolean cont = true;
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("outputFile"));
+            while(cont){
+            	Order obj = null;
+                try {
+                	obj = (Order) ois.readObject();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                  if(obj != null)
+                     ret.add(obj);
+                  else
+                     cont = false;
+               }
+            ois.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return ret;
 	}
 }
---
+++ ProjectControl.java
@@ -0,0 +1,209 @@
+package Project;
+
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+
+public class ProjectControl {
+	@FXML private RadioButton Chef;
+	@FXML private TextField ID;
+	@FXML private RadioButton ProcessingAgent;
+	@FXML private RadioButton Student;
+	@FXML private Button loginButton;
+	@FXML private ToggleGroup selections;
+	@FXML private RadioButton Cheese;
+	@FXML private RadioButton Pepperoni;
+	@FXML private RadioButton Hawaiian;
+	@FXML private RadioButton Margherita;
+	@FXML private Button pizzaTypeButton;
+	@FXML private CheckBox Mushrooms;
+	@FXML private CheckBox Pepperonis;
+	@FXML private CheckBox Peppers;
+	@FXML private CheckBox Onions;
+	@FXML private CheckBox Pineapple;
+	@FXML private CheckBox Olives;
+	@FXML private Button pizzaToppingsButton;
+	@FXML private TextField text;
+	@FXML private Button orderConfirmButton;
+	@FXML private TextField order1;
+	@FXML private Button refreshButton;
+	@FXML private Button clearButton;
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	int id = 0;
+
+	public void validate(ActionEvent e) throws IOException {
+		String chosenUser = "";
+		if(Chef.isSelected()) {
+			chosenUser = "Chef";
+		} else if(ProcessingAgent.isSelected()) {
+			chosenUser = "ProcessingAgent";
+		} else if(Student.isSelected()) {
+			chosenUser = "Student";
+		}
+		
+		Student s = new Student();
+		Chef chef = new Chef();
+		ProcessingAgent processingAgent = new ProcessingAgent();
+		
+		boolean checkID;
+		
+		id = Integer.parseInt(ID.getText());
+
+		if(chosenUser == "Student") {
+			checkID = Notifications.checkValidIDStudent(ID.getText());
+			if(ID.getText().length() != 10) 
+				checkID = false;
+			
+			if(checkID) {
+				s.setID(id);
+				root = FXMLLoader.load(getClass().getResource("PizzaSelection.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				stage.setUserData(s);
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			} else {
+				root = FXMLLoader.load(getClass().getResource("retryLoginScreen.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			}
+		}
+		if(chosenUser == "Chef") {
+			checkID = Notifications.checkValidIDChef(ID.getText());
+			if(ID.getText().length() != 10) 
+				checkID = false;
+			
+			if(checkID) {
+				chef.setID(id);
+				root = FXMLLoader.load(getClass().getResource("PizzaSelection.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				stage.setUserData(s);
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			} else {
+				root = FXMLLoader.load(getClass().getResource("retryLoginScreen.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			}
+		}
+		if(chosenUser == "ProcessingAgent") {
+			checkID = Notifications.checkValidIDProcessingAgent(ID.getText());
+			if(ID.getText().length() != 10) 
+				checkID = false;
+			
+			if(checkID) {
+				processingAgent.setID(id);
+				root = FXMLLoader.load(getClass().getResource("ProcessingAgentScreen.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				stage.setUserData(processingAgent);
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			} else {
+				root = FXMLLoader.load(getClass().getResource("retryLoginScreen.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			}
+		}
+		
+	}
+	
+	public void confirmPizzaType(ActionEvent e) throws IOException {
+		Node node = (Node) e.getSource();
+		Stage stage = (Stage) node.getScene().getWindow();
+		Student s = (Student) stage.getUserData();
+		
+		Pizza pizza = new Pizza();
+		if(Cheese.isSelected()) {
+			pizza.setPizzaType("Cheese");
+		} else if(Pepperoni.isSelected()) {
+			pizza.setPizzaType("Pepperoni");
+		} else if(Hawaiian.isSelected()) {
+			pizza.setPizzaType("Hawaiian");
+		} else if(Margherita.isSelected()) {
+			pizza.setPizzaType("Margherita");
+		}
+		
+		Order order = new Order(s, pizza, "ACCEPTED", "0");
+
+		root = FXMLLoader.load(getClass().getResource("PizzaToppingSelection.fxml"));
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		stage.setUserData(order);
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show(); 
+	}
+	
+	public void confirmPizzaToppings(ActionEvent e) throws IOException {
+		Node node = (Node) e.getSource();
+		Stage stage = (Stage) node.getScene().getWindow();
+		Order o = (Order) stage.getUserData();
+		
+		ArrayList<String> toppings = new ArrayList<String>();
+		if(Mushrooms.isSelected()) 
+			toppings.add("Mushrooms");
+		if(Pepperonis.isSelected()) 
+			toppings.add("Pepperoni");
+		if(Peppers.isSelected()) 
+			toppings.add("Peppers");
+		if(Onions.isSelected()) 
+			toppings.add("Onions");
+		if(Pineapple.isSelected())
+			toppings.add("Pineapple");
+		if(Olives.isSelected())
+			toppings.add("Olives");
+		
+		o.getPizza().setPizzaToppings(toppings);
+				
+		root = FXMLLoader.load(getClass().getResource("OrderDetails.fxml"));
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		stage.setUserData(o);
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show(); 
+	}
+	
+	public void displayOrderStatus(ActionEvent e) throws IOException {
+		Node node = (Node) e.getSource();
+		Stage stage = (Stage) node.getScene().getWindow();
+		Order o = (Order) stage.getUserData();
+		
+		String time = text.getText();
+		
+		o.setTime(time);
+		
+		System.out.println(o.toString());
+		
+		//Notifications.writeOrder(o);
+		root = FXMLLoader.load(getClass().getResource("acceptScreen.fxml"));
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show(); 
+	}
+	
+	public void refresh(ActionEvent e) throws IOException {
+		order1.setText("Order 1");
+	}
+	
+	public void pushButton(ActionEvent e) throws IOException {
+		order1.clear();
+	}
+	
+}
